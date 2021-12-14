7d2a4fa7b41fa9263c21aad32ffae22366c9cee8
Pavi2010 <pavithrapapu96@gmail.com>
2021-12-14 10:01:50 +0530

    Message

---
+++ src/Employee.java
@@ -0,0 +1,21 @@
+
+public class Employee {
+	
+	private void empname() {
+	System.out.println("My Name is :Pavithra");
+	
+	}
+	
+	private void empid() {
+	System.out.println("Employee ID :234905");
+	}
+
+
+    public static void main(String[] args) {
+    	
+    	Employee e = new Employee();
+    	
+    	e.empname();
+    	e.empid();
+    }
+}
